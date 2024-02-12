a5ec5a9eed9701959b576c2eb7ee2444c8f589b2
Lauren <lauren.m.lavelle@wmich.edu>
2024-02-12 18:51:49 -0500

    fixed delete post route

--- tech-news-java-api/src/main/resources/static/js/delete-post.js
+++ tech-news-java-api/src/main/resources/static/js/delete-post.js
@@ -1,22 +1,21 @@
 async function deleteFormHandler(event) {
-console.log(event)
 event.preventDefault();
-alert("bet asjdkdks")
 
   const id = window.location.toString().split('/')[
     window.location.toString().split('/').length - 1
   ];
 
   const response = await fetch(`/api/posts/${id}`, {
-    method: 'DELE'
+    method: 'DELETE'
   });
-
- document.location.replace('/dashboard')
-//  if (response.status===204) {
-//    document.location.replace('/dashboard')
-//  } else {
-//    alert(response.statusText);
-//  }
+//console.log(response)
+//console.log(response.ok)
+// document.location.replace('/dashboard')
+  if (response.ok) {
+    document.location.replace('/dashboard')
+  } else {
+    alert(response.statusText);
+  }
 }
 
 document.querySelector('.delete-post-btn').addEventListener('click', deleteFormHandler);
