30322e3a3684b267fc72ec95932879bf7db30d31
Lauren <lauren.m.lavelle@wmich.edu>
2024-03-04 20:11:44 -0500

    fixed edit post so both postText and title update

---
+++ tech-news-java-api/src/main/resources/static/js/edit-post.js
@@ -0,0 +1,29 @@
+const postTitle= document.querySelector('#edit-title');
+const postText= document.querySelector('#edit-postText');
+
+async function editPostHandler(event) {
+  event.preventDefault();
+
+  const id = window.location.toString().split('/')[
+    window.location.toString().split('/').length - 1
+  ];
+
+  const response = await fetch(`/api/posts/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({
+        title: postTitle.value,
+        postText: postText.value
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+//
+//  if (response.ok) {
+//    document.location.reload();
+//  } else {
+//    alert(response.statusText);
+//  }
+}
+
+document.querySelector('.edit-post').addEventListener('click', editPostHandler);
